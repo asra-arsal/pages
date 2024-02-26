df2b273ec393817c953a263a7e5d2a4760bf5406
Asra & Arsal <126740113+asra-arsal@users.noreply.github.com>
2024-02-27 00:55:36 +0100

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,20 +1,15 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set "currentPath=%~dp0"
+for %%A in ("..") do (
+set "prevDir=%%~nxA"
+)
 
-for %%A in ("%currentPath%") do set "currDir=%%~nxA"
+for %%A in (".") do (
+set "currDir=%%~nxA"
+)
 
-set "parentPath=%currentPath%\.."
-
-for %%A in ("%parentPath%") do set "prevDir=%%~nxA"
-
-
-set "grandParentPath=%parentPath%\.."
-
-for %%A in ("%grandParentPath%") do set "grandParentDir=%%~nxA"
-
-set "cmd=pm2 start app.js -n !grandParentDir!-!prevDir!"
+set "cmd=pm2 start main.js -n !prevDir!-!currDir!"
 
 echo Command: !cmd!
 
